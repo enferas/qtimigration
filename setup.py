f19a97662396bf7c0f4b12d1812787b643b8b9fc
steve.w.lay <steve.w.lay@19e0bdba-bd96-11dd-afb1-a7db9ebf18e3>
2011-01-22 15:14:19 +0000

    Added PyAssess source to project for safe keeping

--- setup.py
+++ setup.py
@@ -1,51 +1,29 @@
-"""
-This script is designed to support creation of the binary
-distributions of the QTI migration tool.
+#!/usr/bin/env python
 
-Usage (Mac OS X):
-	python setup.py py2app
+import sys
 
-Usage (Windows):
-	python setup.py py2exe
-"""
-
-import sys, os
-
-# Modify the path to ensure that our lib modules are picked up
-wd=os.getcwd()
-sys.path.append(os.path.join(wd,"lib"))
-
-mainscript='migrate.py'
-
-try:
-	import vobject
-except:
-	print "ERROR: vobject should be installed when creating binary executables"
-	sys.exit(1)
-
-if sys.platform == 'darwin':
-	from setuptools import setup
-	extra_options = dict(
-		setup_requires=['py2app'],
-		app=[mainscript],
-		# Cross-platform applications generally expect sys.argv to
-		# be used for opening files.
-		options=dict(py2app=dict(argv_emulation=True)),
-	)
-elif sys.platform == 'win32':
-	from distutils.core import setup
-	import py2exe
-	extra_options = dict(
-		setup_requires=['py2exe'],
-		app=[mainscript],
-		console=[dict(script='migrate.py',icon_resources=[(1,"QTIMigrate.ico"),(2,"folder.ico"),(3,"readme.ico")])]
-	)
+if sys.hexversion<0x02020000:
+	print "PyAssess requires Python Version 2.2 (or greater)"
 else:
-	print "ERROR: build for unsupported target"
-	sys.exit(1)
+	from distutils.core import setup
+	from distutils.sysconfig import get_python_lib
+	pkgBase=get_python_lib()
 	
-setup(
-	name="QTIMigration",
-	data_files = ['IMSLogo.bmp'],
-	**extra_options
-)
+	setup(name="PyAssess",
+		version="20051127",
+		description="PyAssess Library",
+		author="Steve Lay",
+		author_email="S.W.Lay@ucles-red.cam.ac.uk",
+		packages=['PyAssess',
+			'PyAssess.ieee',
+			'PyAssess.ietf',
+			'PyAssess.ims',
+			'PyAssess.ims.qti',
+			'PyAssess.iso',
+			'PyAssess.unicode',
+			'PyAssess.w3c'],
+		data_files=[(pkgBase+'/PyAssess/unicode',['PyAssess/unicode/ucd_tables.pck'])]
+		# package_data={'PyAssess.unicode':['ucd_tables.pck']} - wait for Python 2.4
+		)
+	
+

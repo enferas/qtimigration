c6300464d7c956a1242e610e5715476166f6e711
steve.w.lay <steve.w.lay@19e0bdba-bd96-11dd-afb1-a7db9ebf18e3>
2011-06-11 16:53:00 +0000

    Tagging QTIMigration-1.9.20110609 Release

--- pyslet/pyslet/__init__.py
+++
@@ -1,8 +0,0 @@
-"""A package of useful modules for implementing standards for learning, education and training
-
-"""
-
-# Define the list of modules to import when clients use:
-# from pyslet import *
-__all__ = [  ]
-
